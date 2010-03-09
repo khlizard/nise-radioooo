982f0c69e6e8377711a7e8740477d5d9a76d0864
khtokage <khtokage-git@vanu.jp>
2010-03-09 20:36:54 +0900

    add coding

--- src/shorturl/main.py
+++ src/shorturl/main.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# -*- coding:utf-8 -*-
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -29,8 +15,8 @@ class MainHandler(webapp.RequestHandler):
 
   def get(self):
     baseuri = 'http://radioooclone.vanu.jp/'
+    use_method = '-'
     chre = re.compile('[\w\-]{4,}', re.IGNORECASE)
-    
     mt = chre.match(self.request.query_string)
     
     if mt:
