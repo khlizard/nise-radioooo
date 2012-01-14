392f51efa853732bdf20020fb9cc75bd47c1f734
khtokage <khtokage-dev@vanu.jp>
2012-01-14 15:43:16 +0900

    use version (git tag)

--- app.rb
+++ app.rb
@@ -4,30 +4,32 @@ require 'rubygems'
 require 'sinatra'
 
 before do
-  # enginxだったかVarnishだったかの仕様
-  @host = env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
+  # heroku or fluxflex
+  @host    = env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
+  @version = File.read('version').strip.gsub("-",".").sub(/.{6}$/,"")
 end
 
 get '/' do
   content_type 'text/html', :charset => 'utf-8'
   cache_control :public, :max_age => 3600
-  File.read 'public/index.html'
+  File.read('public/index.html').gsub('$Version$', @version)
 end
 
 get '/counter/:hashtag' do
-  'そのうちカウンターとか'
+  'not yet'
 end
 
-get '/env' do
-  content_type 'text/plain', :charset => 'utf-8'
-  ENV.each.sort.map{|k,v| [k,v.inspect]*" => "} * "\n\n"
-end
 
-get '/version' do
-  content_type 'text/plain', :charset => 'utf-8'
-  File.read 'version'
-end
 
-#get '/shorturl/:videoid' do
-#  '今はt.coあるしいらんよね'
-#end
+# dev
+if ENV['RACK_ENV'] == 'development' then
+  get '/env' do
+    content_type 'text/plain', :charset => 'utf-8'
+    ENV.each.sort.map{|k,v| [k,v.inspect]*" => "} * "\n\n"
+  end
+
+  get '/version' do
+    content_type 'text/plain', :charset => 'utf-8'
+    @version
+  end
+end
